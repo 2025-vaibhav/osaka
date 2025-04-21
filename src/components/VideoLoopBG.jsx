770e8589fd07c23e5ee04ac143b87743bd9f425c
2025-vaibhav <vaibhav.dell.2003@gmail.com>
2025-04-21 18:11:43 +0530

    _

--- src/components/VideoLoopBG.jsx
+++ src/components/VideoLoopBG.jsx
@@ -11,11 +11,11 @@ const VideoLoopBG = () => {
         disablePictureInPicture
         className="top-0 left-0 w-full h-full object-cover -z-10 absolute bg-black pointer-events-none"
       >
-        <source src="/loop.mp4" type="video/mp4" />
+        <source src="/v1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <img
-        src="black.png"
+        src="i.png"
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-150"
       />
     </>
